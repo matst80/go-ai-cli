e77ed5e604de7b6b7edbcaf3431edf6632cad249
matst80 <mats.tornberg@gmail.com>
2026-03-04 08:21:55 +0100

    fix

--- pkg/terminal/simple.go
+++ pkg/terminal/simple.go
@@ -40,7 +40,7 @@ func RunSimpleSession(client *ollama.Client, req ollama.ChatRequest) (string, []
 
 		for msg := range workerCh {
 			if msg.Error != nil {
-				return "", msg.Error
+				return "", req.Messages, msg.Error
 			}
 			if msg.ReasoningContent != "" {
 				if assistantMsg.ReasoningContent == "" {
